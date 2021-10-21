16d47ba4174decadf069d9e5d91d6dca3f11e7f9
Hamza <Hamza5955>
2021-10-21 12:11:36 +0200

    init

---
+++ index.php
@@ -0,0 +1,15 @@
+/*- è definita una classe ‘Movie’
+   => all'interno della classe sono dichiarate delle variabili d'istanza
+   => all'interno della classe è definito un costruttore
+   => all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà */
+<?php
+
+    class Movie {
+        public $name;
+        public $director;
+        public $time;
+        public $relisedate;
+        public $writers;
+        public $tipe;
+    }
